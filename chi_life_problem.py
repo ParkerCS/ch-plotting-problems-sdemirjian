c660dbeb12175f7b377db0890fad5e91ab29ec64
sdemirjian <sdemirjian@fwparker.org>
2017-03-17 14:44:44 -0500

    yee

--- chi_life_problem.py
+++ chi_life_problem.py
@@ -11,6 +11,80 @@
 #11  Comment your code as always.
 
 # Note:  If you would like to present something different than the above for your graph using this dataset, just let me know your intentions before you start and I will do my best to support you.
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
+
+life_data = []
+file = open("chi_life_expectancy.txt", 'r')
+
+reader = csv.reader(file, delimiter='\t')
+
+for line in reader:
+    life_data.append(line)
+
+print(life_data)
+
+def name(library):
+    name = life_data[library][1]
+    return name
+
+def expectancy(library):
+    try:
+        amount = float(life_data[library][8])
+        return amount
+    except:
+        print(life_data[library][8])
+
+xval = []
+yval = []
+patch_list = []
+
+biggest = 2
+biggest_name = ""
+biggest_position = 0
+smallest = 70
+smallest_name = ""
+smallest_position = 0
+for i in range(77):
+    total = expectancy(i+1)
+    if total > biggest:
+        biggest = total
+        biggest_name = name(i)
+        biggest_position = i
+    elif total < smallest:
+        smallest = total
+        smallest_name = name(i)
+        smallest_position = i
+    else:
+        pass
+
+for i in range(77):
+    xval.append(name(i+1))
+    if name(i+1) == biggest_name:
+        patch_list.append(Patch(color="orange", label="Maximum Life Expectancy"))
+    elif name(i+1) == smallest_name:
+        patch_list.append(Patch(color="green", label="Minimum Life Expectancy"))
+    else:
+        pass
+
+for i in range(77):
+    yval.append(expectancy(i+1))
+
+print(yval)
+
+plt.figure(1,tight_layout=True,figsize=(12,5))
+plt.legend(handles=patch_list)
+plt.bar(np.arange(len(yval)), yval,.6)
+plt.bar(biggest_position, biggest, .5, .5)
+plt.bar(smallest_position, smallest, .5, .5)
 
 
+plt.title("2010 Life Expectancy in Chicago by District")
+plt.xlabel("District")
+plt.ylabel("Average Life Expectancy (years)")
 
+plt.xticks(np.arange(len(yval)), xval,rotation=-90)
+
+plt.show()
--- chi_pub_lib_problem.py
+++ chi_pub_lib_problem.py
@@ -7,3 +7,118 @@
 # plot the total visitors on the y and month on the x.  You will have 4 separate lines (total and 3 libraries)
 # add a legend
 # label axes, title the graph
+
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.patches import Patch
+
+lib_data = []
+file = open("chilib_visitors_2016", 'r')
+
+reader = csv.reader(file, delimiter='\t')
+
+for line in reader:
+    lib_data.append(line)
+
+print(lib_data)
+
+headers = lib_data[0]
+lib_data = lib_data[1:]
+print(lib_data)
+
+def total_per_month(library):
+    total = 0
+    for i in range(len(lib_data)):
+        total += int(lib_data[i][library + 1])
+    return total
+
+def total_visitors(library):
+    total = int(lib_data[library][-1])
+    return total
+
+def lib_name(library):
+    name = lib_data[library][0]
+    return name
+
+xval = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+yval = []
+patch_list = []
+
+for i in range(12):
+    yval.append(total_per_month(i))
+patch_list.append(Patch(color="purple", label="Total Visitors per Month"))
+
+biggest = 3
+medium = 2
+smallest = 1
+for i in range(79):
+    total = total_visitors(i)
+    if total > biggest:
+        biggest = total
+    elif total > medium and total < biggest:
+        medium = total
+    elif total > smallest and total < medium:
+        smallest = total
+    else:
+        pass
+
+lib1 = "yee"
+lib2 = "yee"
+lib3 = "yee"
+
+for i in range(79):
+    if total_visitors(i) == biggest:
+        lib1 = lib_name(i)
+        patch_list.append(Patch(color="blue",label=lib_name(i)))
+    elif total_visitors(i) == medium:
+        lib2 = lib_name(i)
+        patch_list.append(Patch(color="red",label=lib_name(i)))
+    elif total_visitors(i) == smallest:
+        lib3 = lib_name(i)
+        patch_list.append(Patch(color="yellow",label=lib_name(i)))
+    else:
+        pass
+
+print(lib1)
+print(lib2)
+print(lib3)
+
+print(biggest)
+print(medium)
+print(smallest)
+
+print(patch_list)
+
+lib1y = []
+lib2y = []
+lib3y = []
+
+for i in range(79):
+    if total_visitors(i) == biggest:
+        for k in range(12):
+            lib1y.append(lib_data[i][k + 1])
+    if total_visitors(i) == medium:
+        for k in range(12):
+            lib2y.append(lib_data[i][k+1])
+    if total_visitors(i) == smallest:
+        for k in range(12):
+            lib3y.append(lib_data[i][k+1])
+
+print(lib1y, lib2y, lib3y)
+
+plt.figure(1,tight_layout=True)
+plt.legend(handles=patch_list)
+plt.plot(np.arange(12), yval,"purple")
+plt.plot(np.arange(12), lib1y,"blue")
+plt.plot(np.arange(12), lib2y,"red")
+plt.plot(np.arange(12), lib3y,"yellow")
+
+plt.title("Total Library Visitors per Month")
+plt.xlabel("Months")
+plt.ylabel("Total Visitors")
+
+#Label our x-axis
+plt.xticks(np.arange(len(yval)), xval,rotation=-45)
+
+plt.show()
